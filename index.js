3f21fdca2c7ffbdc99424aa5c661660b98b0aa26
Ege Çelikçi <deathconsciousness@tutanota.com>
2021-05-14 18:23:58 +0300

    modesta v3 changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ https://github.com/egecelikci/ahegao
     <meta content="Start your day with a lovely ahegao ~" property="og:description"/>
     <meta content="Start your day with a lovely ahegao ~" name="description"/>
   </head>
-  <body class="github-font-standard">
+  <body class="dark-theme-github">
     <section class="fullscreen">
       <div class="background" style="background-image: url('/random');"></div>
       <div class="center-object">
@@ -28,9 +28,9 @@ https://github.com/egecelikci/ahegao
           <span class="undertitle">Start your day with a lovely ahegao ~<br><i>Images in API: <b>${data.length}</b></i></span>
         </section>
         <div class="buttons">
-          <a href="random" class="btn animation white black-text" target="_blank">Random</a>
-          <a href="api" class="btn animation white black-text" target="_blank">API</a>
-          <a href="https://github.com/egecelikci/ahegao" class="btn animation white black-text" target="_blank">GitHub</a>
+          <a href="random" class="btn animation white-bg black-text" target="_blank">Random</a>
+          <a href="api" class="btn animation white-bg black-text" target="_blank">API</a>
+          <a href="https://github.com/egecelikci/ahegao" class="btn animation white-bg black-text" target="_blank">GitHub</a>
         </div>
       </div>
     </section>
